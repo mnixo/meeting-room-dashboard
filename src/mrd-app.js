9f57ff93f2f8a27ab067c9abcaf605060ca5fc59
Miguel Nixo <mnixo@nuxeo.com>
2019-05-11 19:02:13 +0100

    add mrd-calendar-api

--- src/mrd-app.js
+++ src/mrd-app.js
@@ -1,10 +1,12 @@
 import { LitElement, html } from 'lit-element';
+import './mrd-calendar-api';
 
 class MRDApp extends LitElement {
 
   render() {
     return html`
       <p>Meeting Room Dashboard</p>
+      <mrd-calendar-api></mrd-calendar-api>
     `;
   }
 
