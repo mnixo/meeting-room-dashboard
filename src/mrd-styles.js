0da250a73f189cf4804f5089c73bb0b5c395f48a
Miguel Nixo <mnixo@nuxeo.com>
2019-05-12 01:01:12 +0100

    cleanup mrd-styles

--- src/mrd-styles.js
+++ src/mrd-styles.js
@@ -2,16 +2,6 @@ import { html } from 'lit-element';
 
 export class MRDStyles {
 
-  static get appHeader() {
-    return html`
-      <style>
-        app-header {
-          font-family: 'Roboto', 'Noto', sans-serif;
-        }
-      </style>
-    `;
-  }
-
   static get paperButton() {
     return html`
       <style>
