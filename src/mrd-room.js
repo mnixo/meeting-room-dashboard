5ecbc7a48d0e068da5f40bbb1203c38d5e3be8ae
Miguel Nixo <mnixo@nuxeo.com>
2019-08-01 20:10:18 +0100

    add moment

--- src/mrd-room.js
+++ src/mrd-room.js
@@ -1,22 +1,29 @@
 import { html } from 'lit-element';
 import { MRDElement } from './mrd-element';
+import moment from 'moment';
 
 class MRDRoom extends MRDElement {
 
   static get properties() {
     return {
       calendar: Object,
+      _message: String,
+      _state: String,
     };
   }
 
   constructor() {
     super();
     this.calendar = null;
+    this._message = null;
+    this._state = null;
   }
 
   render() {
     return html`
       <div>${this.calendar.label}</div>
+      <div>${this._state}</div>
+      <div>${this._message}</div>
     `;
   }
 
@@ -29,8 +36,21 @@ class MRDRoom extends MRDElement {
       'singleEvents': true,
       'maxResults': 10,
       'orderBy': 'startTime'
-    }).then(function(response) {
+    }).then(response => {
       console.log(response.result.items);
+      if (response.result.items.length) {
+        const event = response.result.items[0];
+        const eventStart = moment(event.start.dateTime);
+        if (eventStart.isSameOrBefore(moment.now())) {
+          const eventEnd = moment(event.end.dateTime);
+          this._state = `Busy (free ${eventEnd.fromNow()})`;
+        } else {
+          this._state = `Free (busy ${eventStart.fromNow()})`;
+        }
+        this._message = event.summary;
+      } else {
+        this._message = 'no events';
+      }
     });
   }
 
