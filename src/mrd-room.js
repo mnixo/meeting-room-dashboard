5ec005079c05393c7c143f5d48665e05ac2cb894
Miguel Nixo <mnixo@nuxeo.com>
2019-08-02 00:02:02 +0100

    show consecutive events

--- src/mrd-room.js
+++ src/mrd-room.js
@@ -7,7 +7,7 @@ class MRDRoom extends MRDElement {
   static get properties() {
     return {
       calendar: Object,
-      _message: String,
+      _events: Array,
       _state: String,
     };
   }
@@ -15,7 +15,7 @@ class MRDRoom extends MRDElement {
   constructor() {
     super();
     this.calendar = null;
-    this._message = null;
+    this._events = [];
     this._state = null;
   }
 
@@ -31,13 +31,20 @@ class MRDRoom extends MRDElement {
       <paper-card>
         <div>${this.calendar.label}</div>
         <div>${this._state}</div>
-        <div>${this._message}</div>
+        ${this._renderEvents(this._events)}
       </paper-card>
     `;
   }
 
+  _renderEvents(events) {
+    return events.map(event => {
+      return html`
+        <paper-card style="margin-top: 0.5em;">${event.summary}</paper-card>
+      `;
+    });
+  }
+
   triggerUpdate() {
-    console.log('updating ' + this.calendar.label);
     gapi.client.calendar.events.list({
       'calendarId': this.calendar.id,
       'timeMin': (new Date()).toISOString(),
@@ -48,17 +55,44 @@ class MRDRoom extends MRDElement {
     }).then(response => {
       console.log(response.result.items);
       if (response.result.items.length) {
-        const event = response.result.items[0];
-        const eventStart = moment(event.start.dateTime);
-        if (eventStart.isSameOrBefore(moment.now())) {
-          const eventEnd = moment(event.end.dateTime);
-          this._state = `Busy (free ${eventEnd.fromNow()})`;
+
+        // list of the next consecutive events
+        const consecutiveEvents = [];
+        // start date of the first event of the next consecutive events list
+        let consecutiveEventsStart = null;
+        // end date of the last event of the next consecutive events list
+        let consecutiveEventsEnd = null;
+        response.result.items.forEach(event => {
+          const eventStart = moment(event.start.dateTime ? event.start.dateTime : event.start.date);
+          const eventEnd = moment(event.end.dateTime ? event.end.dateTime : event.end.date);
+          if (consecutiveEvents.length) {
+            // the next consecutive events list is not empty
+            if (eventStart.isSameOrBefore(consecutiveEventsEnd)) {
+              // this event starts before the end date of the last event of the list, so add it
+              consecutiveEvents.push(event);
+              if (eventEnd.isAfter(consecutiveEventsEnd)) {
+                // this event ends after the end date of the last event of the list, so update this date
+                consecutiveEventsEnd = eventEnd;
+              }
+            }
+          } else {
+            // the next consecutive events list is empty, so let's start with this one
+            consecutiveEvents.push(event);
+            consecutiveEventsStart = eventStart;
+            consecutiveEventsEnd = eventEnd;
+          }
+        });
+
+        if (consecutiveEventsStart.isSameOrBefore(moment.now())) {
+          // current time is after the start of the consecutive events
+          this._state = `Busy (free ${consecutiveEventsEnd.fromNow()})`;
         } else {
-          this._state = `Free (busy ${eventStart.fromNow()})`;
+          // current time is before the start of the consecutive events
+          this._state = `Free (busy ${consecutiveEventsStart.fromNow()})`;
         }
-        this._message = event.summary;
-      } else {
-        this._message = 'no events';
+
+        this._events = consecutiveEvents;
+
       }
     });
   }
