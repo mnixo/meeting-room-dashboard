d05ed320af40d995aee4a8846541270141354f7d
Miguel Nixo <mnixo@nuxeo.com>
2019-08-06 08:48:21 +0100

    add message to each event

--- src/mrd-room.js
+++ src/mrd-room.js
@@ -38,10 +38,77 @@ class MRDRoom extends MRDElement {
 
   _renderEvents(events) {
     return events.map(event => {
+      // start date and time
+      const start = moment(event.start.dateTime ? event.start.dateTime : event.start.date);
+      const startDate = start.format('D/M/YYYY');
+      const startTime = start.format('H:mm');
+      // end date and time
+      const end = moment(event.end.dateTime ? event.end.dateTime : event.end.date);
+      let endDate = end.format('D/M/YYYY');
+      const endTime = end.format('H:mm');
+      // current date
+      const now = moment();
+      const nowDate = now.format('D/M/YYYY');
+      // tomorrow's date
+      const tomorrowDate = now.add(1, 'days').format('D/M/YYYY');
+      // message that is shown
+      let message;
+
+      const termsMap = {};
+      termsMap[nowDate] = 'today';
+      termsMap[tomorrowDate] = 'tomorrow';
+
+      if (startDate === nowDate && endDate === nowDate) {
+        // starts and ends today
+        message = `Today from ${startTime} to ${endTime}`;
+      } else if (startDate === tomorrowDate && endDate === tomorrowDate) {
+        // starts and ends tomorrow
+        message = `Tomorrow from ${startTime} to ${endTime}`;
+      } else if (startDate === endDate) {
+        // starts and ends on the same day, but far in the future
+        message = `${startDate} from ${startTime} to ${endTime}`;
+      } else {
+        // starts and ends in different days
+        if (startTime === '0:00' && endTime === '0:00') {
+          // all day event (the last day doesn't actually count)
+          end.subtract(1, 'minutes');
+          endDate = end.format('D/M/YYYY');
+          if (startDate === endDate) {
+            // only 1 day long
+            message = `${this._mapValue(startDate, termsMap, true)} (all day)`;
+          } else {
+            // at least 2 days long
+            const startTerm = this._mapValue(startDate, termsMap, true);
+            const endTerm = this._mapValue(endDate, termsMap);
+            const joiningTerm = end.diff(start, 'days') === 1 ? 'and' : 'until';
+            message = `${startTerm} ${joiningTerm} ${endTerm}`;
+          }
+        } else {
+          // not all day event, although it starts and ends on different days
+          const startTerm = this._mapValue(startDate, termsMap, true);
+          const endTerm = this._mapValue(endDate, termsMap);
+          const joiningTerm = end.diff(start, 'days') === 1 ? 'and' : 'until';
+          message = `${startTerm} (${startTime}) ${joiningTerm} ${endTerm} (${endTime})`;
+        }
+      }
+
       return html`
-        <paper-card style="margin-top: 0.5em;">${event.summary}</paper-card>
+        <paper-card style="margin-top: 0.5em;">
+          <div>${event.summary}</div>         
+          <div>${message}</div>
+        </paper-card>
       `;
     });
+  }
+
+  /**
+   * Returns the mapping of a given value in a given map.
+   * If the map has no entry for the value, the value is returned instead.
+   * If set, the `capitalize` option will capitalize the returning value.
+   */
+  _mapValue(value, map, capitalize) {
+    const term = map[value] ? map[value] : value;
+    return capitalize ? term.charAt(0).toUpperCase() + term.slice(1) : term;
   }
 
   triggerUpdate() {
