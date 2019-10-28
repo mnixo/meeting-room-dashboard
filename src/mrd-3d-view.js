a95722601a9f6463a8085f8a358109564ee61e3b
Miguel Nixo <mnixo@nuxeo.com>
2019-10-28 23:04:03 +0000

    simplify 3d view

--- src/mrd-3d-view.js
+++ src/mrd-3d-view.js
@@ -1,11 +1,6 @@
 import { html } from 'lit-element';
 import * as THREE from 'three';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
-import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass';
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
-import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader';
 import { MRDElement } from './mrd-element';
 
 class MRD3dView extends MRDElement {
@@ -13,9 +8,8 @@ class MRD3dView extends MRDElement {
   static get properties() {
     return {
       _camera: Object,
-      _composer: Object,
-      _outlineMap: Object,
       _renderer: Object,
+      _roomMap: Object,
       _scene: Object,
       _sceneRotation: Number,
       settings: Object,
@@ -24,7 +18,7 @@ class MRD3dView extends MRDElement {
 
   constructor() {
     super();
-    this._outlineMap = {};
+    this._roomMap = {};
     this._sceneRotation = 0;
     this.settings = null;
   }
@@ -52,7 +46,7 @@ class MRD3dView extends MRDElement {
 
     // setup scene
     this._scene = new THREE.Scene();
-    this._scene.rotation.x = 0.75;
+    this._scene.rotation.x = 0.65;
 
     // setup camera
     this._camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 1000);
@@ -66,14 +60,6 @@ class MRD3dView extends MRDElement {
     this._renderer.setSize(width, height);
     canvas.appendChild(this._renderer.domElement);
 
-    // setup composer
-    this._composer = new EffectComposer(this._renderer);
-    const renderPass = new RenderPass(this._scene, this._camera);
-    this._composer.addPass(renderPass);
-    const effectFXAA = new ShaderPass(FXAAShader);
-    effectFXAA.uniforms['resolution'].value.set(1/width, 1/height);
-    this._composer.addPass(effectFXAA);
-
     // load model
     const modelUrl = this.settings && this.settings.modelUrl;
     if (modelUrl) {
@@ -81,22 +67,16 @@ class MRD3dView extends MRDElement {
       loader.load(modelUrl, model => {
         model.traverse(subModel => {
           if (subModel instanceof THREE.Mesh) {
-            subModel.material = new THREE.MeshBasicMaterial({
-              color: 0x0f0f0f,
-              opacity: 0.1,
+            const geometry = new THREE.EdgesGeometry(subModel.geometry);
+            const material = new THREE.LineBasicMaterial({
+              color: 0xeeeeee,
+              opacity: 1.0,
             });
-            const outlinePass = new OutlinePass(new THREE.Vector2(width, height), this._scene, this._camera);
-            outlinePass.edgeStrength = 2;
-            outlinePass.edgeGlow = 1;
-            outlinePass.pulsePeriod = 0;
-            outlinePass.visibleEdgeColor.set(0x000000);
-            outlinePass.hiddenEdgeColor.set(0x000000);
-            outlinePass.selectedObjects = [ subModel ];
-            this._composer.addPass(outlinePass);
-            this._outlineMap[subModel.name] = outlinePass;
+            const mesh = new THREE.LineSegments(geometry, material);
+            this._scene.add(mesh);
+            this._roomMap[subModel.name] = mesh;
           }
         });
-        this._scene.add(model);
       });
     }
 
@@ -108,7 +88,7 @@ class MRD3dView extends MRDElement {
     requestAnimationFrame(this._animate.bind(this));
     this._sceneRotation += 0.01;
     this._scene.rotation.y = 0.25 * Math.sin(this._sceneRotation) + Math.PI;
-    this._composer.render();
+    this._renderer.render(this._scene, this._camera);
   }
 
   _onResize() {
@@ -118,17 +98,15 @@ class MRD3dView extends MRDElement {
     this._camera.aspect = width / height;
     this._camera.updateProjectionMatrix();
     this._renderer.setSize(width, height);
-    this._composer.setSize(width, height);
   }
 
   updateRoom(room) {
     console.log(room);
-    const outlinePass = this._outlineMap[room.calendar.label];
-    if (!outlinePass) {
+    const roomMesh = this._roomMap[room.calendar.label];
+    if (!roomMesh) {
       return;
     }
-    outlinePass.visibleEdgeColor.set(room.status === 'busy' ? 0xff0000 : 0x00ff00);
-    outlinePass.hiddenEdgeColor.set(room.status === 'busy' ? 0xff0000 : 0x00ff00);
+    roomMesh.material.color.setHex(room.status === 'busy' ? 0xff0000 : 0x00ff00);
   }
 
 }
